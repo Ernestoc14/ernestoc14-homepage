6a0c165e116213fc322c27d47e5d8a0df6f7c940
Ernestoc14 <descreed34@hotmail.com>
2024-07-30 23:51:07 -0500

    Feat: Changing the Logo in website

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f2dc5149b1767051"; }
+/******/ 		__webpack_require__.h = function() { return "297bda797caa4db5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
